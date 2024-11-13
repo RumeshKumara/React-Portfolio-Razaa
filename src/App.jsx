a80324f61388f4d0669cd179dfbfc33c4baf8d3e
rumesh <rumeshk066@gmail.com>
2024-11-13 22:01:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+//  import React from 'react'
+ 
+ const App = () => {
+   return (
+     <div>
+       <h1 className="text-4xl text-red-600 bg-slate-900">Hello World!</h1>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
