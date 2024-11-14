8563af00a0c9d995f5dd51fc5fe2c6589add4442
rumesh <rumeshk066@gmail.com>
2024-11-14 21:09:18 +0530

    test-commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@
    return (
      <div>
        <h1 className="text-4xl text-red-600 bg-slate-900">Hello World!</h1>
+       <p className="text-xl text-gray-600">This is a simple React application using Vite.</p>
      </div>
    )
  }
