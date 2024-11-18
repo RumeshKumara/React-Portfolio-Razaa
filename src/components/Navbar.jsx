e6ca892b2e4a820d51473adc71b423819fed6c6a
rumesh <rumeshk066@gmail.com>
2024-11-18 14:56:46 +0530

    create and add icons navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,45 @@
 // import React from 'react'
+import { BsGithub, BsLinkedin, BsTwitterX, BsYoutube } from "react-icons/bs"
 
 const Navbar = () => {
   return (
-    <div>
-      Nav
+    <div className="fixed top-0 flex items-center justify-between w-full px-16 py-5 text-white border-b border-b-gray-700 bg-blue-950/30 backdrop-blur-md md:justify-evenly">
+      <a href="#home" className="text-3xl font-semibold text-transparent transition-all duration-300 bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text opacity-80 hover:opacity-100">
+        #Razaa</a>
+
+        <ul className="hidden gap-10 md:flex">
+          <a href="#home" className="transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100">
+            <li>Home</li>
+          </a>
+
+          <a href="#tech" className="transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100">
+            <li>Tech</li>
+          </a>
+
+          <a href="#projects" className="transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100">
+            <li>Projects</li>
+          </a>
+
+          <a href="#contact" className="transition-all duration-300 cursor-pointer opacity-70 hover:opacity-100">
+            <li>Contact</li>
+          </a>
+        </ul>
+
+        <ul className="hidden gap-5 md:flex">
+          <li className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:text-red-500 hover:opacity-100">
+            <BsYoutube />
+          </li>
+          <li className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:text-blue-500 hover:opacity-100">
+            <BsLinkedin />
+          </li>
+          <li className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:text-gray-500 hover:opacity-100">
+            <BsTwitterX />
+          </li>
+          <li className="text-xl transition-all duration-300 cursor-pointer opacity-70 hover:text-purple-500 hover:opacity-100">
+            <BsGithub />
+          </li>
+
+        </ul>
     </div>
   )
 }
