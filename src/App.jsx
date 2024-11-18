17c1949efc30df1b2f1d29586930a7e5a39727a6
rumesh <rumeshk066@gmail.com>
2024-11-18 14:04:25 +0530

    add bg

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Tech from "./components/Tech"
  const App = () => {
    return (
      <>
+     <div className="fixed -z-10 min-h-screen w-full bg-[#000000] bg-[radial-gradient(#ffffff33_1px,#00091d_1px)] bg-[size:20px_20px]"></div>
 
       <main className="flex flex-col items-center px-4 md:px-8">
 
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,7 +4,6 @@ const Project = () => {
   return (
     <div>
       project react
-      <h1 className="text-lg text-red-600 bg-green-600">Hello World!</h1>
     </div>
   )
 }
