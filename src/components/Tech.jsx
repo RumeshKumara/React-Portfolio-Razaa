5d5e593216a4d2dda47881fcd38d3d1d108222cc
rumesh <rumeshk066@gmail.com>
2024-11-30 20:55:00 +0530

    add technology icon

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,16 @@
 // import React from 'react'
+import { BiLogoTypescript } from "react-icons/bi";
 
 const Tech = () => {
   return (
-    <div>
-      tech
+    <div id="tech" className="flex min-h-[70vh] w-full flex-col items-center justify-center gap-16 md:gap-32">
+      <h1 className="text-4xl font-light text-white md:text-6xl">Technologies</h1>
+
+      <div className="flex flex-wrap items-center justify-center gap-10 p-5">
+        <div>
+          <BiLogoTypescript className="cursor-pointer text-[80px] text-sky-500 transition-all duration-300 hover:-translate-y-5 sm:text-[100px] md:text-[120px]"/>
+        </div>
+      </div>
     </div>
   )
 }
