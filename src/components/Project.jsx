6925cc347a8f0df688d25ab6fccc3be99e78f716
rumesh <rumeshk066@gmail.com>
2024-12-02 14:41:52 +0530

    commit .. animation add

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,6 +4,9 @@ import image2 from "/public/1.png";
 import image3 from "/public/1.png";
 import image4 from "/public/1.png";
 import image5 from "/public/1.png";
+
+import { motion } from "framer-motion";
+
 // import PropTypes from "prop-types";
 
 const projectsData = [
@@ -40,8 +43,23 @@ const projectsData = [
 
 ]
 
+const ScrollReveal = ({children}) => {
+  return (
+    <motion.div
+    initial={{opacity: 0, y:100}}
+    whileInView={{opacity:1, y:0}}
+    viewport={{once:true}}
+    transition={{duration:0.8}}
+    >
+      {children}
+    </motion.div>
+  )
+  
+}
+
 const ProjectCard = ({ project }) => {
   return (
+    <ScrollReveal>
     <div className="flex flex-col items-center gap-8 md:flow-row md:gap-24">
       <img src={project.image} alt="" className="w-full cursor-pointer rounded-2xl transition-all duration-300 hover:scale-105 md:w-[300px]" />
 
@@ -60,12 +78,17 @@ const ProjectCard = ({ project }) => {
         </div>
       </div>
     </div>
+    </ScrollReveal>
   )
 }
 const Project = () => {
   return (
     <div id="projects" className="flex flex-col items-center justify-center w-full min-h-screen gap-16 p-4 md:px-14 md:py-24 ">
+
+    <ScrollReveal>
       <h1 className="text-4xl font-light text-white md:text-6xl">My Projects</h1>
+    </ScrollReveal>
+
 
       <div className="flex w-full max-w-[1000px] flex-col gap-16 text-white">
         {
